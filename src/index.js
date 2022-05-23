526d13f94786f3adb8b630b99afe93e564a0563c
Vladislav Ishchenko <boshik64new@gmail.com>
2022-05-23 17:25:14 +0300

    5lesson

--- src/index.js
+++ src/index.js
@@ -2,16 +2,41 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import { BrowserRouter } from 'react-router-dom';
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+
+const initialState = {
+  count: 0
+}
+
+const reducer = (state = initialState, action) => {
+  switch (action.type) {
+    case 'increase':
+      return {
+        ...state,
+        count: state.count + 1
+      }
+    case 'decrease':
+      return {
+        ...state,
+        count: state.count - 1
+      }
+    default:
+      return state
+  }
+}
+
+const store = createStore(reducer)
 root.render(
-  <BrowserRouter>
+
+  <Provider store={store}>
     <React.StrictMode>
       <App />
     </React.StrictMode>
-  </BrowserRouter>
+  </Provider>
 
 );
 
